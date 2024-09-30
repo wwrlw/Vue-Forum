0cee7302218f5240093b1363a0cc46dc4a64c7a2
timas <timas3811@gmail.com>
2024-09-30 02:46:05 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import AboutComponent from "@/components/AboutComponent.vue";
+import HomeComponent from "@/components/HomeComponent.vue"
+export default {
+  components: {
+    AboutComponent,
+    HomeComponent,
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <AboutComponent/>
+    <HomeComponent/>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
